2a8a49961881594998a6ac4ed235f61424e8cfe1
astrotzka <166855326+astrotzka@users.noreply.github.com>
2025-10-01 10:00:40 +0000

    lab 1

--- A.java
+++ A.java
@@ -1,9 +1,12 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("   ****");
-        System.out.println("  ******");
-        System.out.println(" ***  ***");
-        System.out.println("**********");
+        System.out.println("      ****");
+        System.out.println("     ******");
+        System.out.println("    ***  ***");
+        System.out.println("   **********");
+        System.out.println("  ************");
+        System.out.println(" ***        ***");
+        System.out.println("***          ***");
         
     }
     
---
+++ epl.java
@@ -0,0 +1,15 @@
+public class epl {
+    public static void main(String[]args){
+        System.out.println("-----------------------------------------");
+        System.out.println("|# |Team\t |P |W |D |L |Points\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|1 |Chealsea  |7 |5 |1 |1 |36\t\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|2 |Arsenal   |7 |4 |1 |2 |29\t\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|2 |Man City  |7 |4 |1 |2 |29\t\t|");
+        System.out.println("-----------------------------------------");
+        System.out.println("|4 |Liverpool |7 |4 |0 |3 |28\t\t|");
+        System.out.println("-----------------------------------------");
+    }
+}
